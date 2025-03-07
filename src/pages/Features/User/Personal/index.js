f8933661c3a905dad69c21e47ae6c6e67d8b2db4
phat ho <phatho1508@gmail.com>
2025-03-07 11:46:02 +0700

    07/03/2025

--- src/pages/Features/User/Personal/index.js
+++ src/pages/Features/User/Personal/index.js
@@ -1,38 +1,66 @@
-import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import api from '~/api/api';
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare, faRectangleXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import styles from './Personal.module.scss';
 import { useUserMenuItems, useAdminMenuItems } from '~/staticData';
 import { Link } from 'react-router-dom';
+import Button from '~/components/Button';
 const cx = classNames.bind(styles);
 const Personal = () => {
-    const [userData, setUserData] = useState({});
     const userMenuItems = useUserMenuItems();
+    const [username, setUsername] = useState('');
+    const [username2, setUsername2] = useState(''); // this one just set only once
+
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [address, setAddress] = useState('');
+    const [createdAt, setCreatedAt] = useState('');
+    const [editingField, setEditingField] = useState([]);
+    const profileFields = [
+        { label: 'Username', value: username, setValue: setUsername, field: 'username' },
+        { label: 'Email', value: email, setValue: setEmail, field: 'email' },
+        { label: 'Phone', value: phone, setValue: setPhone, field: 'phone' },
+        { label: 'Address', value: address, setValue: setAddress, field: 'address' },
+    ];
     const getUserData = async () => {
         try {
             const response = await api.get('/user/detail');
-            setUserData(response.data.data);
+            setUsername(response.data.data.username);
+            setUsername2(response.data.data.username);
+            setEmail(response.data.data.email);
+            setPhone(response.data.data.phone);
+            setAddress(response.data.data.address);
+            setCreatedAt(response.data.data.createdAt);
         } catch (error) {
             console.log(error);
         }
     };
+    const displayEditField = (field) => {
+        setEditingField((prev) => [...prev, field]);
+    };
+    const cancelEditField = (field) => {
+        setEditingField((prev) => prev.filter((item) => !(item == field)));
+    };
+    // const update
     useEffect(() => {
         getUserData();
     }, []);
 
     return (
         <div className={cx('wraper', 'row-nowrap')}>
-            <div className={cx('col-3', '.flex-column')}>
-                <div className={cx('resume', 'row-nowrap')}>
-                    <img src="https://muaban.net/images/account/avatar-default.png"></img>
-                    <div>
-                        <h3>{userData.username}</h3>
+            <div className={cx('col-3', 'flex-column')}>
+                <div className={cx('resume', 'flex-column')}>
+                    <div className="col">
+                        <img src="https://muaban.net/images/account/avatar-default.png"></img>
+                    </div>
+
+                    <div className="col">
+                        <h3>{username2}</h3>
                         <p>
                             tham gia từ{' '}
-                            {new Date(userData.createdAt).toLocaleDateString('vi-VN', {
+                            {new Date(createdAt).toLocaleDateString('vi-VN', {
                                 month: 'long',
                                 year: 'numeric',
                             })}{' '}
@@ -64,55 +92,48 @@ const Personal = () => {
             <div className={cx('col-full')}>
                 <ul className={cx('infor-detail', 'flex-column')}>
                     <h2>Thông tin tài khoản</h2>
+                    {profileFields.map((item, index) => {
+                        return (
+                            <li className="row-nowrap">
+                                <div className="col-3">
+                                    <p className={cx('label')}>{item.label} </p>
+                                </div>
+                                <div className={cx('col-full', 'value')}>
+                                    <div className={cx('row-space-between')}>
+                                        {editingField.includes(item.label) ? (
+                                            <>
+                                                <input
+                                                    type="text"
+                                                    value={item.value}
+                                                    onChange={(e) => item.setValue(e.target.value)}
+                                                    className={cx('input-field', 'col-full')}
+                                                />
+                                                <div
+                                                    onClick={() => {
+                                                        cancelEditField(item.label);
+                                                    }}
+                                                >
+                                                    <FontAwesomeIcon icon={faRectangleXmark} />
+                                                </div>
+                                            </>
+                                        ) : (
+                                            <>
+                                                <p>{item.value}</p>
 
-                    <li className="row-nowrap">
-                        <div className="col-3">
-                            <label className={cx('label')}>Username:</label>
-                        </div>
-                        <div className="col-full">
-                            <div className={cx('row-space-between')}>
-                                <p>{userData.username}</p>
-                                <FontAwesomeIcon icon={faPenToSquare} />
-                            </div>
-                        </div>
-                    </li>
-
-                    <li className="row-nowrap">
-                        <div className="col-3">
-                            <label className={cx('label')}>Email:</label>
-                        </div>
-                        <div className="col-full">
-                            <div className={cx('row-space-between')}>
-                                <p>{userData.email}</p>
-                                <FontAwesomeIcon icon={faPenToSquare} />
-                            </div>
-                        </div>
-                    </li>
-
-                    <li className="row-nowrap">
-                        <div className="col-3">
-                            <label className={cx('label')}>Phone:</label>
-                        </div>
-                        <div className="col-full">
-                            <div className={cx('row-space-between')}>
-                                <p>{userData.phone}</p>
-                                <FontAwesomeIcon icon={faPenToSquare} />
-                            </div>
-                        </div>
-                    </li>
-
-                    <li className="row-nowrap">
-                        <div className="col-3">
-                            <label className={cx('label')}>Address:</label>
-                        </div>
-                        <div className="col-full">
-                            <div className={cx('row-space-between')}>
-                                <p>{userData.address}</p>
-                                <FontAwesomeIcon icon={faPenToSquare} />
-                            </div>
-                        </div>
-                    </li>
+                                                <div onClick={() => displayEditField(item.label)}>
+                                                    <FontAwesomeIcon icon={faPenToSquare} />
+                                                </div>
+                                            </>
+                                        )}
+                                    </div>
+                                </div>
+                            </li>
+                        );
+                    })}
                 </ul>
+                <div style={{ marginTop: '1.6rem' }}>
+                    {editingField.length > 0 ? <Button primary>Cập nhật</Button> : ''}
+                </div>
             </div>
         </div>
     );
